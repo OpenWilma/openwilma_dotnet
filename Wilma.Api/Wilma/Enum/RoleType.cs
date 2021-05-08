d58bd362e8e700f579c298892e8aa704251041e3
PaulusParssinen <paulus.parssinen01@edupori.fi>
2021-05-08 13:08:09 +0300

    Add rest of the user role types

--- Wilma.Api/Wilma/Enum/RoleType.cs
+++ Wilma.Api/Wilma/Enum/RoleType.cs
@@ -11,6 +11,12 @@
         Guardian= 4,
         Instructor = 5,
         Board = 6,
-        Username = 7
+        Username = 7,
+        Coordinator = 8,
+        TrainingPlace = 9,
+        
+        Class = 100,
+        Course = 101,
+        Unknown = 1000
     }
 }
